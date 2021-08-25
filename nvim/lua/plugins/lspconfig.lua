4c2417b67195f87b1b98b9598a52fc033521104d
fantasmo <gheadley27@gmail.com>
2021-08-25 21:40:22 +0100

    disable virtual text

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,1 +1,7 @@
-require('lspconfig').pyright.setup{}
+vim.lsp.diagnostic.show_line_diagnostics()
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+})
+
+require('lspconfig').pyright.setup{
+}
