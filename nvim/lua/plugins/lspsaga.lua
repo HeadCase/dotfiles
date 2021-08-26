769c769e7dc670a9400b03754342daccd285bf9e
headcase <gheadley27@gmail.com>
2021-08-26 10:02:01 +0100

    re-disable lspsaga optional config

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -1,4 +1,6 @@
-require('lspsaga').init_lsp_saga({
+local saga = require('lspsaga')
+
+--[[ .init_lsp_saga({
   code_action_icon = " ",
   definition_preview_icon = "  ",
   dianostic_header_icon = "   ",
@@ -13,4 +15,6 @@ require('lspsaga').init_lsp_saga({
   max_preview_lines = 10, -- preview lines of lsp_finder and definition preview
 	finder_action_keys = { open = 'o', vsplit = 's',split = 'i',
 												 quit = 'q',scroll_down = '<C-f>', scroll_up = '<C-b>'}
-})
+}) ]]
+
+saga.init_lsp_saga()
