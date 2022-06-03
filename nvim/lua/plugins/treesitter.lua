1b31664dff147aa3076102d1c38524888cc6b755
Greg Headley <gheadley27@gmail.com>
2022-06-03 21:11:35 +0000

    treesitter; packer updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,11 @@
 require'nvim-treesitter.configs'.setup {
   -- Modules and its options go here
-  ensure_installed = 'maintained',
+  ensure_installed = { 'bash', 'c', 'cmake', 'css', 'html', 'javascript',
+  'json', 'json5', 'jsonc', 'julia', 'latex', 'lua', 'make', 'markdown',
+  'python', 'r', 'rst', 'typescript', 'yaml', }
+,
   highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = { enable = true },
 }
+
