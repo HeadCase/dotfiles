8ee7e1d06e71981307a7f276197b6d8467b7c050
Greg Headley <gheadley27@gmail.com>
2023-01-07 17:40:28 +0000

    plugin setup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,7 +31,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { 'pyright', 'sumneko_lua'}
 
 for _, lsp in pairs(servers) do
