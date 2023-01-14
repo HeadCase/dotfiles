a39c34a7ee4125c86b4834ceaa5311664842f12b
fantasmo <gheadley27@gmail.com>
2023-01-14 21:21:06 +0000

    switch off treesitter jumps

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -49,7 +49,7 @@ require("nvim-treesitter.configs").setup({
 		},
 		move = {
 			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
+			set_jumps = false, -- whether to set jumps in the jumplist
 			goto_next_start = {
 				["]m"] = "@function.outer",
 				["]]"] = { query = "@class.outer", desc = "Next class start" },
