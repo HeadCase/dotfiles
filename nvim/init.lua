71c47dda7868680e5a41be084e0af8af17c8ce50
headcase <gheadley27@gmail.com>
2024-10-09 22:19:37 +0000

    chore(upgrade): moved minimal to lazy vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,2 @@
-require('headcase')
-require("lazy").setup("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      path_display = { "filename_first" },
+      layout_strategy = "flex",
+    },
+  },
+}
